13a5ce4acd2f5e18dff3c94c9add462d74b43860
Nitisha Holkar <nitishaholkar.work@gmail.com>
2022-10-12 11:42:21 +0530

    Add board list

--- src/components/board-list/BoardList.tsx
+++ src/components/board-list/BoardList.tsx
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from '../ui/card/Card';
 import './BoardList.css';
 
+interface BoardType {
+  id: string;
+  name: string;
+}
+
 export default function BoardList() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [boardList, setBoardList] = useState<BoardType[]>();
+
+  useEffect(() => {
+    fetch('http://localhost:8080/v1/board')
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setBoardList(data);
+      });
+  }, []);
+
+  const renderBoardList = boardList?.map((board, i) => (
+    <Card key={board.id}>{board.name}</Card>
+  ));
+
   return (
     <>
-      <section className="board-content">
-        <Card></Card>
-      </section>
+      <section className="board-content">{renderBoardList}</section>
     </>
   );
 }
