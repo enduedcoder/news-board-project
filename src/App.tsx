92ea8adacf82e357364ef02dddbc3c4000aa4335
Nitisha Holkar <nitishaholkar.work@gmail.com>
2022-10-12 10:56:04 +0530

    Add route setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
 import { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
 import './App.css';
 import LoginContext from './context/LoginContext';
 import Login from './components/login/Login';
+import BoardList from './components/board-list/BoardList';
 
 function App() {
   const [email, setEmail] = useState('');
 
   return (
     <LoginContext.Provider value={{ email, setEmail }}>
-      <div>
-        <p>In app: {email}</p>
-
-        <Login />
-      </div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/boards" element={<BoardList />} />
+        </Routes>
+      </Router>
     </LoginContext.Provider>
   );
 }
