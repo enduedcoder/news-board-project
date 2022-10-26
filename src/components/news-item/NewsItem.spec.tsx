8b7d309f2ab4e4ac581d552f2b63321a990f4d6c
Nitisha Holkar <nitishaholkar.work@gmail.com>
2022-10-26 10:03:55 +0530

    Add newsitem component

---
+++ src/components/news-item/NewsItem.spec.tsx
@@ -0,0 +1,67 @@
+import { render, screen } from '@testing-library/react';
+import NewsItem from './NewsItem';
+
+const WRAPPER_TEST_ID_SELECTOR = 'news-card-test-id';
+
+const defaultTestProps = {
+  newsData: [
+    {
+      CreatedAt: 'some date',
+      author: 'test-author',
+      boardId: 'test-board-id',
+      description: 'some description',
+      id: 'some-id',
+      imageURL: 'some url',
+      status: 'some-status',
+      title: 'some title',
+    },
+  ],
+  onDeleteNews: (arg: any) => {
+    console.log(arg);
+  },
+};
+
+describe('NewsItem', () => {
+  it('should render content', () => {
+    render(<NewsItem {...defaultTestProps} />);
+    const wrapper = screen.getByTestId(WRAPPER_TEST_ID_SELECTOR);
+
+    expect(wrapper).toBeInTheDocument();
+  });
+
+  it('should render title', () => {
+    render(<NewsItem {...defaultTestProps} />);
+    const titleText = screen.getByText(
+      defaultTestProps.newsData[0].title
+    );
+
+    expect(titleText).toBeInTheDocument();
+  });
+
+  it('should render author', () => {
+    render(<NewsItem {...defaultTestProps} />);
+    const authorText = screen.getByText(
+      defaultTestProps.newsData[0].author
+    );
+
+    expect(authorText).toBeInTheDocument();
+  });
+
+  it('should render description', () => {
+    render(<NewsItem {...defaultTestProps} />);
+    const descriptionText = screen.getByText(
+      defaultTestProps.newsData[0].description
+    );
+
+    expect(descriptionText).toBeInTheDocument();
+  });
+
+  it('should render status', () => {
+    render(<NewsItem {...defaultTestProps} />);
+    const statusText = screen.queryAllByText(
+      defaultTestProps.newsData[0].status
+    );
+
+    expect(statusText.length).toBeGreaterThan(0);
+  });
+});
--- src/components/news-item/NewsItem.tsx
+++ src/components/news-item/NewsItem.tsx
@@ -1,38 +1,170 @@
-import React from 'react';
-import './NewsItem.css';
-
-interface NewsDetailsType {
-  CreatedAt: string;
-  author: string;
-  boardId: string;
-  description: string;
-  id: string;
-  imageURL: string;
-  status: string;
-  title: string;
-}
+import React, { useEffect, useState } from 'react';
+import AddNews from '../add-news/AddNews';
+import Button from '../ui/button/Button';
+import Select from '../ui/select/Select';
+import './NewsItem.scss';
+import { API_BASE_URL, NewsDetailsType } from '../utils';
 
 interface News {
   newsData: NewsDetailsType[];
+  onDeleteNews: (args: NewsDetailsType) => void;
 }
 
-export default function NewsItem({ newsData }: News) {
+const options = [
+  {
+    label: 'Draft',
+    value: 'draft',
+  },
+  {
+    label: 'Published',
+    value: 'published',
+  },
+  {
+    label: 'Archived',
+    value: 'archive',
+  },
+];
+
+export default function NewsItem({ newsData, onDeleteNews }: News) {
+  const [id, setId] = useState<string>(null!);
+  const [newsDataArray, setNewsDataArray] =
+    useState<NewsDetailsType[]>(newsData);
+  const [editing, setEditing] = useState(false);
+
+  useEffect(() => {
+    setNewsDataArray(newsData);
+  }, [newsData]);
+
+  const handleEditNews = (newsDataItem: NewsDetailsType) => {
+    setEditing(true);
+
+    if (newsDataItem.id) {
+      setId(newsDataItem.id);
+    }
+  };
+
+  const handleDeleteNews = (data: NewsDetailsType) => {
+    fetch(`${API_BASE_URL}/news/${data.id}`, {
+      method: 'DELETE',
+    }).then(() => {
+      onDeleteNews(data);
+    });
+  };
+
+  const handleStatusChange = (
+    e: any,
+    newsDataItem: NewsDetailsType
+  ) => {
+    fetch(
+      `${API_BASE_URL}/news/${newsDataItem.id}/${e.target.value}`,
+      {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json',
+        },
+      }
+    ).then((response) => {
+      setNewsDataArray((prevState) => {
+        return prevState.map((newsItem) =>
+          newsItem.id === newsDataItem.id
+            ? { ...newsDataItem, status: e.target.value }
+            : newsItem
+        );
+      });
+    });
+  };
+
+  const renderUpdatedNews = (data: NewsDetailsType) => {
+    setNewsDataArray((prevState) => {
+      return prevState.map((newsItem) =>
+        newsItem.id === data.id ? data : newsItem
+      );
+    });
+    setEditing(false);
+  };
+
+  const toggleForm = (value: any) => {
+    setEditing(!value);
+  };
+
   return (
     <>
-      {newsData.map((newsDataItem) => {
+      {newsDataArray.map((newsDataItem) => {
         return (
-          <div className="news-card" key={newsDataItem.id}>
-            <div className="news-header">
-              <h5>{newsDataItem.status}</h5>
+          <div
+            className="news-card"
+            key={newsDataItem.id}
+            data-testid="news-card-test-id"
+          >
+            <div
+              className="news-header"
+              data-testid="news-header-test-id"
+            >
+              <h4>{newsDataItem.status}</h4>
             </div>
+
+            <Select
+              values={options}
+              selectedValue={newsDataItem.status}
+              onSelectChange={(e) =>
+                handleStatusChange(e, newsDataItem)
+              }
+            />
+
             <div className="news-body">
-              <h5 className="new-title">{newsDataItem.title}</h5>
-              <p>{newsDataItem.boardId}</p>
-              <p>{newsDataItem.author}</p>
-              <p>{newsDataItem.description}</p>
-              <button className="news-delete">Delete</button>
-              <button className="news-edit">Delete</button>
+              <img
+                src={newsDataItem.imageURL}
+                alt={`${newsDataItem.title}`}
+              />
+              <div className="news-details">
+                <p className="news-title">
+                  <b>Title : </b>
+                  {newsDataItem.title}
+                </p>
+
+                <p className="news-author">
+                  <b>Author : </b>
+                  {newsDataItem.author}
+                </p>
+
+                <p className="news-description">
+                  <b>Description : </b>
+                  {newsDataItem.description}
+                </p>
+
+                <p className="news-status">
+                  <b>STATUS : </b>
+                  {newsDataItem.status}
+                </p>
+
+                <div className="new-item-button-container">
+                  <Button
+                    classes="delete-btn"
+                    onButtonClick={(e) =>
+                      handleDeleteNews(newsDataItem)
+                    }
+                  >
+                    Delete
+                  </Button>
+
+                  <Button
+                    onButtonClick={(e) =>
+                      handleEditNews(newsDataItem)
+                    }
+                  >
+                    Edit
+                  </Button>
+                </div>
+              </div>
             </div>
+            {editing && id === newsDataItem.id && (
+              <AddNews
+                isEdit={editing}
+                data={newsDataItem}
+                addNews={(data) => renderUpdatedNews(data)}
+                onToggleForm={toggleForm}
+              />
+            )}
           </div>
         );
       })}
